2c2c441ea9e800908a46a41228777f4e70707dec
parkhongjoon <9030joon@naver.com>
2025-07-04 15:26:39 +0900

    first commit

---
+++ src/main/java/com/zerobase/spendingalertservice/SpendingAlertServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zerobase.spendingalertservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingAlertServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendingAlertServiceApplication.class, args);
+    }
+}
